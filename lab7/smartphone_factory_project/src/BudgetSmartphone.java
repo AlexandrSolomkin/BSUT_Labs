102ee28640dc4b972739606a07684b0276119394
Your Name <you@example.com>
2025-04-03 11:19:27 +0300

    lab7 is ready

---
+++ lab7/smartphone_factory_project/src/BudgetSmartphone.java
@@ -0,0 +1,6 @@
+public class BudgetSmartphone extends Smartphone {
+    @Override
+    public String getSpecifications() {
+        return "Model: Budget Y\nProcessor: Snapdragon 4 Gen 1\nRAM: 4GB\nStorage: 64GB\nCamera: 48MP Single Lens\nBattery: 4000mAh";
+    }
+}
